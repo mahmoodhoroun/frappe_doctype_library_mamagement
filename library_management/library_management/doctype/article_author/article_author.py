1c46b3995d748017d15ae92653552a9c9dacd9d5
mahmoodhoroun <87674051+mahmoodhoroun@users.noreply.github.com>
2023-01-16 16:03:58 +0200

    Add files via upload

---
+++ library_management/library_management/doctype/article_author/article_author.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Mahmood and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ArticleAuthor(Document):
+	pass
